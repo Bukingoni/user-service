1f1998acdf5d65a3b2413c7fd27ac6bff5cbc8a5
Svetozar Vulin <svetozar.vulin@gmail.com>
2023-05-14 18:57:05 +0200

    refactor: code formatting

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const userRoutes = require('../modules/user/routes')
+const userRoutes = require("../modules/user/routes");
 
-router.use('/users', userRoutes);
+router.use("/users", userRoutes);
 
 module.exports = router;
